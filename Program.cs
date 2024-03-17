5a73f54bb900451aa0ee3338323f059cb343219a
arifinilham04 <ailham983@gmail.com>
2024-03-17 23:12:13 +0700

    TPMOD5_1302223061

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMOD5_1302223061
+{
+    class DataGeneric<T>
+    {
+        T Data { get; set; }
+        public DataGeneric(T data)
+        {
+            Data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine($"tersimpan data sebagai berikut: {Data}");
+        }
+    }
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            DataGeneric<string> stringData = new DataGeneric<string>("Y");
+            stringData.PrintData();
+            DataGeneric<string> nimData = new DataGeneric<string>("1302223061");
+            nimData.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
